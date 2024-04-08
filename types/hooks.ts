a96bf205b3d1bd777f44d0655a25b8fd534ba30d
Richard Pentecost <richardpentecost1@gmail.com>
2024-04-08 15:01:13 +0100

    customise navbar to use metamask accounts

--- types/hooks.ts
+++ types/hooks.ts
@@ -7,19 +7,24 @@ export type Web3Dependencies = {
   provider: BrowserProvider;
   contract: Contract;
   ethereum: MetaMaskInpageProvider;
+  isLoading: boolean;
 };
 
-export type CryptoHookFactory<D = any, P = any> = {
-  (d: Partial<Web3Dependencies>): CryptoHandlerHook<D, P>;
+export type CryptoHookFactory<D = any, R = any, P = any> = {
+  (d: Partial<Web3Dependencies>): CryptoHandlerHook<D, R, P>;
 };
 
-export type CryptoHandlerHook<D = any, P = any> = (
-  params: P
-) => CryptoSWRResponse<D>;
+export type CryptoHandlerHook<D = any, R = any, P = any> = (
+  params?: P
+) => CryptoSWRResponse<D, R>;
 
-export type CryptoSWRResponse<D = any> = SWRResponse<D>;
+export type CryptoSWRResponse<D = any, R = any> = SWRResponse<D> & R;
 
 // This is one type representation of the 3 types above
 // export type CryptoHookFactory<D = any, P = any> = {
-//   (d: Partial<Web3Dependencies>): (params: P) => SWRResponse<D>;
+//   (d: Partial<Web3Dependencies>): (params?: P) => SWRResponse<D>;
 // };
+
+// D = Data
+// R = Response
+// P = Params
